543a58730f9c4b03d7b9cd538d03dc7e512d192b
Gautham Baiju <gauthambaiju040@gmail.com>
2025-07-31 10:06:01 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <h1>apple</h1>
+        <p className='bg-sky-200 text-red-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid provident optio voluptate quos obcaecati maxime numquam facilis quidem deserunt veritatis iure possimus vel quas, nisi nihil architecto eius recusandae blanditiis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid provident optio voluptate quos obcaecati maxime numquam facilis quidem deserunt veritatis iure possimus vel quas, nisi nihil architecto eius recusandae blanditiis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid provident optio voluptate quos obcaecati maxime numquam facilis quidem deserunt veritatis iure possimus vel quas, nisi nihil architecto eius recusandae blanditiis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid provident optio voluptate quos obcaecati maxime numquam facilis quidem deserunt veritatis iure possimus vel quas, nisi nihil architecto eius recusandae blanditiis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid provident optio voluptate quos obcaecati maxime numquam facilis quidem deserunt veritatis iure possimus vel quas, nisi nihil architecto eius recusandae blanditiis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid provident optio voluptate quos obcaecati maxime numquam facilis quidem deserunt veritatis iure possimus vel quas, nisi nihil architecto eius recusandae blanditiis.</p>
+    </>
+  )
+}
+
+export default App
+
